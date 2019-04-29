f009cb6dbaa64756764fa51804a015ee089aab6e
Desu <desu@pokemmo.eu>
2019-04-29 17:12:43 +0000

    CI configuration for Linux armhf

--- src/wrap_memcpy.c
+++ src/wrap_memcpy.c
@@ -2,7 +2,9 @@
 
 void *old_memcpy(void *, const void *, size_t);
 
+#if defined(__i386__) || defined(__x86_64__)
 __asm__(".symver old_memcpy,memcpy@GLIBC_2.2.5");
+#endif
 
 void *__wrap_memcpy(void *dest, const void *src, size_t n) {
 	return old_memcpy(dest, src, n);
