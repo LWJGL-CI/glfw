d65a322351b8db97e9006541523e20b00844dc37
Desu <desu@pokemmo.eu>
2019-04-29 19:45:26 +0000

    add some memcpy wrap symbols for now * aarch64 is memcpy@GLIBC_2.17 on trusty * armhf is memcpy@GLIBC_2.4 on trusty

--- src/wrap_memcpy.c
+++ src/wrap_memcpy.c
@@ -2,8 +2,12 @@
 
 void *old_memcpy(void *, const void *, size_t);
 
-#if defined(__i386__) || defined(__x86_64__)
-__asm__(".symver old_memcpy,memcpy@GLIBC_2.2.5");
+#if defined(__aarch64__)
+  __asm__(".symver old_memcpy,memcpy@GLIBC_2.4");
+#elif defined(__arm__)
+  __asm__(".symver old_memcpy,memcpy@GLIBC_2.4");
+#else
+  __asm__(".symver old_memcpy,memcpy@GLIBC_2.2.5");
 #endif
 
 void *__wrap_memcpy(void *dest, const void *src, size_t n) {
